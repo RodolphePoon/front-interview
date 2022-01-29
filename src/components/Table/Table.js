dc81fa08585fc953f9d53fd523384ca5570c9fc6
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 18:04:25 +0100

    apply css

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -8,8 +8,30 @@ const Logo = ({ iban }) => {
 }
 
 const Table = ({ data, columns }) => {
-  return <table><thead><th></th>{columns.map(({ label }) => <th>{label}</th>)}</thead>
-    <tbody>{data.map(dataRow => <tr><td><Logo iban={dataRow.iban} /></td>{columns.map(({ accessor }) => <td>{dataRow[accessor]}</td>)}</tr>)}</tbody></table>
+  return <table>
+    <thead><th></th>{columns.map(({ label }) => <th>{label}</th>)}</thead>
+    <tbody>
+      {data.map((dataRow) => {
+        let type = ''
+        if (dataRow.status === 'blocked') {
+          type = 'blocked'
+        }
+        if (!dataRow.iban.match('FR')) {
+          type = 'foreign'
+        }
+        if (dataRow.balance.match('-')) {
+          type = 'negative'
+        }
+
+        return (
+          <tr className={type}><td><Logo iban={dataRow.iban} /></td>{columns.map(
+            ({ accessor }) => (<td>{dataRow[accessor]}</td>)
+          )}
+          </tr>)
+      }
+      )}
+    </tbody>
+  </table>
 }
 
 export default Table
