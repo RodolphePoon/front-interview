57e5dfa93573857b93eaa6925113d58ea27688d9
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 19:27:32 +0100

    fix typo iban => ibans

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="iban" element={<Iban />} />
+        <Route path="ibans" element={<Iban />} />
       </Routes>
     </BrowserRouter>
 
