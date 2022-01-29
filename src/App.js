0b420077068335a5f4ca6dc103d5eb4f730520cd
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 17:44:53 +0100

    show data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from "./components/Table"
+import data from "./data.json"
 
+const COLUMNS = ['name', 'status', 'iban', 'balances']
 function App() {
   return (
     <div >
-      <Table />
+      <Table data={data.accounts} columns={COLUMNS} />
     </div>
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,8 @@
 import React from "react"
 
 const Table = ({ data, columns }) => {
-  return <div>
-    table
-  </div>
+  return <table><thead>{columns.map(column => <th>{column}</th>)}</thead>
+    <tbody>{data.map(dataRow => <tr>{columns.map(column => <td>{JSON.stringify(dataRow[column])}</td>)}</tr>)}</tbody></table>
 }
 
 export default Table
