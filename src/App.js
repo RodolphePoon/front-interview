e2660a9628ceef9e1a07be6157f6a44dd5f0aae4
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 17:49:20 +0100

    parse data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
 import Table from "./components/Table"
-import data from "./data.json"
+import rawData from "./data.json"
+import _ from "lodash"
 
-const COLUMNS = ['name', 'status', 'iban', 'balances']
+
+const parseData = (data) => data.map((d) => {
+
+  const parsed = _.chain(d).pick(['name', 'status', 'iban']).value()
+  parsed.balance = _.chain(d).get('balances.0.balanceAmount').map().join(' ').replace('EUR', '€').value()
+  return parsed
+})
+const COLUMNS = [{ label: "Nom de compte", accessor: "name" }, { label: "Status du compte", accessor: "status" }, { label: "IBAN du compte", accessor: "iban" }, { label: "Balance du compte", accessor: "balance" }]
 function App() {
+  const data = parseData(rawData.accounts)
   return (
     <div >
-      <Table data={data.accounts} columns={COLUMNS} />
+      <Table data={data} columns={COLUMNS} />
     </div>
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const Table = ({ data, columns }) => {
-  return <table><thead>{columns.map(column => <th>{column}</th>)}</thead>
-    <tbody>{data.map(dataRow => <tr>{columns.map(column => <td>{JSON.stringify(dataRow[column])}</td>)}</tr>)}</tbody></table>
+  return <table><thead>{columns.map(({ label }) => <th>{label}</th>)}</thead>
+    <tbody>{data.map(dataRow => <tr>{columns.map(({ accessor }) => <td>{dataRow[accessor]}</td>)}</tr>)}</tbody></table>
 }
 
 export default Table
