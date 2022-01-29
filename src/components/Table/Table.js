57378b30bcbd0bb836b3e38c5ad1a1eaa3546cfe
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 17:56:00 +0100

    add logo

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,8 +1,15 @@
 import React from "react"
+import './Table.css'
+
+const Logo = ({ iban }) => {
+  const bank = iban.substring(0, 4)
+
+  return <span className={`logo ${bank}`}>{bank}</span>
+}
 
 const Table = ({ data, columns }) => {
-  return <table><thead>{columns.map(({ label }) => <th>{label}</th>)}</thead>
-    <tbody>{data.map(dataRow => <tr>{columns.map(({ accessor }) => <td>{dataRow[accessor]}</td>)}</tr>)}</tbody></table>
+  return <table><thead><th></th>{columns.map(({ label }) => <th>{label}</th>)}</thead>
+    <tbody>{data.map(dataRow => <tr><td><Logo iban={dataRow.iban} /></td>{columns.map(({ accessor }) => <td>{dataRow[accessor]}</td>)}</tr>)}</tbody></table>
 }
 
 export default Table
