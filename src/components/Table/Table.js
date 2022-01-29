a4db1bcf626e7ceec2c0b087c64e6f6a8d973d3c
rodolphe poon <rodolphe.poon@talan.com>
2022-01-29 18:29:36 +0100

    add pagination

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,37 +1,61 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import './Table.css'
-
 const Logo = ({ iban }) => {
   const bank = iban.substring(0, 4)
 
   return <span className={`logo ${bank}`}>{bank}</span>
 }
 
-const Table = ({ data, columns }) => {
-  return <table>
-    <thead><th></th>{columns.map(({ label }) => <th>{label}</th>)}</thead>
-    <tbody>
-      {data.map((dataRow) => {
-        let type = ''
-        if (dataRow.status === 'blocked') {
-          type = 'blocked'
-        }
-        if (!dataRow.iban.match('FR')) {
-          type = 'foreign'
-        }
-        if (dataRow.balance.match('-')) {
-          type = 'negative'
-        }
+const MAX_PER_PAGE = 2
 
-        return (
-          <tr className={type}><td><Logo iban={dataRow.iban} /></td>{columns.map(
-            ({ accessor }) => (<td>{dataRow[accessor]}</td>)
-          )}
-          </tr>)
-      }
-      )}
-    </tbody>
-  </table>
+const Table = ({ data, columns }) => {
+
+  const [showedData, setShowedData] = useState([])
+
+  const [page, setPage] = useState(1)
+  const maxPage = Math.trunc(data.length / MAX_PER_PAGE) + data.length % MAX_PER_PAGE
+
+
+
+  useEffect(() => {
+    const startIndex = (page - 1) * MAX_PER_PAGE
+    const endIndex = startIndex + MAX_PER_PAGE
+    const newShowedData = data.slice(startIndex, endIndex)
+    setShowedData(newShowedData)
+  }, [data.length, page])
+
+
+
+
+  return <>
+    <table>
+      <thead><th></th>{columns.map(({ label }) => <th>{label}</th>)}</thead>
+      <tbody>
+        {showedData.map((account) => {
+          let type = ''
+          if (account.status === 'blocked') {
+            type = 'blocked'
+          }
+          if (!account.iban.match('FR')) {
+            type = 'foreign'
+          }
+          if (account.balance.match('-')) {
+            type = 'negative'
+          }
+
+          return (
+            <tr className={type}><td><Logo iban={account.iban} /></td>{columns.map(
+              ({ accessor }) => (<td>{account[accessor]}</td>)
+            )}
+            </tr>)
+        }
+        )}
+      </tbody>
+    </table>
+    <button disabled={page === 1} onClick={() => setPage(page - 1)}>-</button>
+    <button disabled={page === maxPage} onClick={() => setPage(page + 1)}>+</button>
+    <span>Page number {page}</span>
+  </>
 }
 
 export default Table
