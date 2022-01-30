3b8decf5fd1d7d8a1847b505b7ff9d76b1f79fea
rodolphe poon <rodolphe.poon@talan.com>
2022-01-30 15:29:18 +0100

    add style

--- src/Feature/Address/Address.js
+++ src/Feature/Address/Address.js
@@ -8,7 +8,6 @@ const Address = () => {
   const [results, setResults] = useState([])
   const [selectedResult, setSelectedResult] = useState({})
 
-
   useEffect(() => {
     const getData = async () => {
       const res = await search(query)
@@ -24,26 +23,23 @@ const Address = () => {
   }
 
   return <>
-    <div>
-
-      <form >
-        <input type='text' onChange={onChange} />
+    <div style={{ display: "flex", flexDirection: "row", padding: "30px", width: "800px" }}>
+      <form className="form-group" style={{ margin: '30px 30px 0 30px', flex: 1 }}>
+        <input type='text' style={{ marginBottom: "10px", width: "100%" }} onChange={onChange} />
         {!!results.length &&
-          <div >
+          <div className="card card-body card-text">
             {results.map((result) => <div onClick={() => { setSelectedResult(result) }}>{result.properties.label}</div>)}
           </div>
         }
       </form>
       <div style={{ flex: 1 }}>
         {!_.isEmpty(selectedResult) &&
-          <div>
-            {_.chain(selectedResult).get('properties').pick(['housenumber', 'street', 'postcode', 'city']).map((value, key) => <div>{key} : {value}</div>).value()}
+          <div className="card card-body card-text" style={{ border: 'none', backgroundColor: "rgb(245,245,245)" }}>
+            {_.chain(selectedResult).get('properties').pick(['housenumber', 'street', 'postcode', 'city']).map((value, key) => <div><span style={{ color: "#6540FE" }}>{key}</span> : <b>{value}</b></div>).value()}
 
           </div>
         }
       </div>
-
-
     </div>
   </>
 }
